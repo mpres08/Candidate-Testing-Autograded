abb9873232610949603e8c627215832dd486453d
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-06-26 18:52:38 -0500

    Wrote question loop

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,31 +3,36 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
+
 let candidateName = " ";
+
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = " ";
 
 
 //TODO: Variables for Part 2
-let questions = 
-  ["Who was the first American woman in space? ",
+
+let questions = [
+  "Who was the first American woman in space? ",
   "True or false: 5 kilometer == 5000 meters? ",
   "(5 + 3)/2 * 10 = ? ",
   "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
   "What is the minimum crew size for the ISS? "];
-let correctAnswers = 
-  ["Sally Ride",
-  "true",
-  "40",
-  "Trajectory",
-  "3"];
-let candidateAnswers;
+let correctAnswers = [
+  "Sally Ride",
+  " true",
+  " 40",
+  " Trajectory",
+  " 3"];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
+
 const input = require('readline-sync');
 candidateName = input.question("Please enter your name: ");
 }
@@ -35,20 +40,17 @@ candidateName = input.question("Please enter your name: ");
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-for(let i = 0; i < questions.length; i++){
-  candidateAnswer = input.question(); 
-  console.log(questions);
+let questionsLength = questions.length;
+for(let i = 0; i < questionsLength; i++) {
+  console.log(questions[i])
+  candidateAnswer = input.question();
 }
-}
+} 
 
 function gradeQuiz(candidateAnswers) {
-
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-  if (candidateAnswer === correctAnswer)
-    console.log("Yes, that is correct!");
-  else {
-    console.log("That is incorrect.");
-  }
+
+console.log(`Your answers: ${candidateAnswers} Correct answer: ${correctAnswers}`);
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
